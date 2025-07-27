2074dd2511f3c15b1e33750c2b13df0769313225
Evan Barbeyto <evann.bar@gmail.com>
2025-07-27 15:49:10 -0700

    fixed class constructors for resto

--- src/resto.js
+++ src/resto.js
@@ -3,33 +3,40 @@ import logo from "./images/Logo-Kasama.png"
 import insta from "./images/instagram.svg"
 import facebook from "./images/facebook.svg"
 
-export const decs = (function() {
-    const logoContainer = document.querySelector('[logo]');
-    const socialsContainer = document.querySelector('[socials]');
-    const logoImg = document.createElement('img');
-    const instaImg = document.createElement('img');
-    const fbImg = document.createElement('img');
-    return{
-        logoContainer,
-        socialsContainer,
-        logoImg,
-        instaImg,
-        fbImg
-    }
-})
-
-const createImg = function(key, value) {
-    key.src = value
-    if(value === instaImg || value === fbImg){
-        decs.socialsContainer.appendChild(key);
-    } else{
-        decs.logoContainer.appendChild(key)
+class Var{
+    get decs(){
+        const logoContainer = document.querySelector('[logo]');
+        const socialsContainer = document.querySelector('[socials]');
+        const logoImg = document.createElement('img');
+        const instaImg = document.createElement('img');
+        const fbImg = document.createElement('img');
+        return{
+            logoContainer,
+            socialsContainer,
+            logoImg,
+            instaImg,
+            fbImg
+        }
     }
 }
 
-createImg(decs.logoImg, logo)
-createImg(decs.instaImg, insta)
-createImg(decs.fbImg, facebook)
+class CreateImg{
+    img(key, value) {
+        key.src = value;
+        if(value === logo){
+            variable.decs.logoContainer.appendChild(key)
+        } else{
+            variable.decs.socialsContainer.appendChild(key);
+        }
+    }
+}
+
+
+export const variable = new Var
+const createImg = new CreateImg
+createImg.img(variable.decs.logoImg, logo)
+createImg.img(variable.decs.instaImg, insta)
+createImg.img(variable.decs.fbImg, facebook)
 
 export const content = document.querySelector('#content');
 const image = document.createElement('img');
